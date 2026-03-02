6b4b0d229d5c391d143e53b06d2078c04c26f3da
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-03-02 23:58:21 +0530

    fs writefile

--- models/home.js
+++ models/home.js
@@ -1,4 +1,7 @@
 const registeredHomes = [];
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../utils/pathUtil");
 module.exports = class Home {
   constructor(houseName, price, location, rating, photoUrl) {
     this.houseName = houseName;
@@ -9,6 +12,14 @@ module.exports = class Home {
   }
   save() {
     registeredHomes.push(this);
+    const homeDataPath = path.join(rootDir, "data", "homes.json");
+    fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (err) => {
+      if (err) {
+        console.log("Error writing home data to file:", err);
+      } else {
+        console.log("Home data saved successfully.");
+      }
+    });
   }
 
   static fetchAll() {
